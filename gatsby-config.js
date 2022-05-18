5fce6f00ed675142bb94b2feb6e9ea19151f2953
Kirsty <kirsty.mcglynn@essentialinsurance.co.uk>
2022-05-18 16:20:54 +0100

    Remove Google config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,37 +9,9 @@ module.exports = {
   pathPrefix: `/quote`,
 
   plugins: [
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        includeInDevelopment: true,
-        trackingIds: [
-          process.env.GATSBY_GOOGLE_CONVERSION_ID,
-          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-        },
-      },
-    },
     `gatsby-plugin-fontawesome-css`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-facebook-pixel`,
-      options: {
-        pixelId: process.env.GATSBY_FACEBOOK_ID,
-      },
-    },
+
     {
       resolve: `gatsby-plugin-hotjar`,
       options: {
@@ -49,4 +21,4 @@ module.exports = {
       },
     },
   ],
-}
+};
