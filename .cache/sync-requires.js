3fe351528eb83d416200de2017784edc4d74e72d
Kirsty <kirsty.mcglynn@essentialinsurance.co.uk>
2022-05-19 09:58:02 +0100

    Add to git ignore

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/kirsty/Documents/essential-form/.cache/dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/kirsty/Documents/essential-form/src/pages/index.js"))),
   "component---src-pages-quote-bp-js": hot(preferDefault(require("/home/kirsty/Documents/essential-form/src/pages/quote-bp.js"))),
   "component---src-pages-quote-js": hot(preferDefault(require("/home/kirsty/Documents/essential-form/src/pages/quote.js"))),
